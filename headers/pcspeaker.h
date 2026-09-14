abaa3a0685e070c791a15b95bc02755b6dc9128d
Xetch <babyroselegaspi01@gmail.com>
2026-09-14 13:02:11 +0800

    Create pcspeaker.h

---
+++ headers/pcspeaker.h
@@ -0,0 +1,8 @@
+#ifndef ZEXTRAS_PCSPEAKER_H
+#define ZEXTRAS_PCSPEAKER_H
+
+void speaker_on(unsigned int freq);
+void speaker_off(void);
+void speaker_beep(unsigned int freq, unsigned int loops);
+
+#endif
